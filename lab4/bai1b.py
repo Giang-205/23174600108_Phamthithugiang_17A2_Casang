2efa55ea0d9ee43d1caf3e0e187e05be57e4f578
18_Phạm Thị Thu Giang_0108 <pttgiang.dhkl17a2hn@sv.uneti.edu.vn>
2024-04-04 20:07:16 +0700

    Create bai1b.py

---
+++ lab4/bai1b.py
@@ -0,0 +1,11 @@
+#Viết chương trình nhập vào các số nguyên dương từ bàn phím cho đến khi tổng các số đã nhập vượt quá 1000
+#Các số chẵn đã nhập
+a = []
+total = 0
+while total <= 1000:
+    n = int(input("Nhập số nguyên dương:"))
+    if n>0:
+        total += n
+        if n % 2 == 0:
+            a.append(n)
+print("Các số chẵn đã nhập:", a)
